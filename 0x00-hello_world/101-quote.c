3e3b0c8225631417a7063697fc74061a50046b00
the-moezzat <dev.moezzat@gmail.com>
2023-06-17 15:02:21 -0700

    feat: task solved

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main is an entry point
- *
- * Return always 0
- **/
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
